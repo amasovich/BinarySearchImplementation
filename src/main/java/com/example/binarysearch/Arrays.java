914b6c02691f6654ea665693c03c195a252e4bdf
Amasovich <amasovich@yandex.ru>
2024-11-27 16:21:56 +0300

    Placed comments in the Arrays class

--- src/main/java/com/example/binarysearch/Arrays.java
+++ src/main/java/com/example/binarysearch/Arrays.java
@@ -118,7 +118,7 @@ public class Arrays {
         return -(low + 1);
     }
 
-    //
+    // Перегруженный метод бинарного поиска для массива int
     private static int genericBinarySearch(int[] a, int fromIndex, int toIndex, int key) {
         int low = fromIndex;
         int high = toIndex - 1;
@@ -138,7 +138,7 @@ public class Arrays {
         return -(low + 1); // элемент не найден
     }
 
-    //
+    // Перегруженный метод бинарного поиска для массива long
     private static int genericBinarySearch(long[] a, int fromIndex, int toIndex, long key) {
         int low = fromIndex;
         int high = toIndex - 1;
@@ -158,7 +158,7 @@ public class Arrays {
         return -(low + 1); // элемент не найден
     }
 
-    //
+    // Перегруженный метод бинарного поиска для массива short
     private static int genericBinarySearch(short[] a, int fromIndex, int toIndex, short key) {
         int low = fromIndex;
         int high = toIndex - 1;
@@ -178,66 +178,82 @@ public class Arrays {
         return -(low + 1);
     }
 
+    // Бинарный поиск для массива byte
     public static int binarySearch(byte[] a, byte key) {
         return genericBinarySearch(a, 0, a.length, key);
     }
 
+    // Бинарный поиск для части массива byte
     public static int binarySearch(byte[] a, int fromIndex, int toIndex, byte key) {
         return genericBinarySearch(a, fromIndex, toIndex, key);
     }
 
+    // Бинарный поиск для массива char
     public static int binarySearch(char[] a, char key) {
         return genericBinarySearch(a, 0, a.length, key);
     }
 
+    // Бинарный поиск для части массива char
     public static int binarySearch(char[] a, int fromIndex, int toIndex, char key) {
         return genericBinarySearch(a, fromIndex, toIndex, key);
     }
 
+    // Бинарный поиск для массива double
     public static int binarySearch(double[] a, double key) {
         return genericBinarySearch(a, 0, a.length, key);
     }
 
+    // Бинарный поиск для части массива double
     public static int binarySearch(double[] a, int fromIndex, int toIndex, double key) {
         return genericBinarySearch(a, fromIndex, toIndex, key);
     }
 
+    // Бинарный поиск для массива float
     public static int binarySearch(float[] a, float key) {
         return genericBinarySearch(a, 0, a.length, key);
     }
 
+    // Бинарный поиск для части массива float
     public static int binarySearch(float[] a, int fromIndex, int toIndex, float key) {
         return genericBinarySearch(a, fromIndex, toIndex, key);
     }
 
+    // Бинарный поиск для массива int
     public static int binarySearch(int[] a, int key) {
         return genericBinarySearch(a, 0, a.length, key);
     }
 
+    // Бинарный поиск для части массива int
     public static int binarySearch(int[] a, int fromIndex, int toIndex, int key) {
         return genericBinarySearch(a, fromIndex, toIndex, key);
     }
 
+    // Бинарный поиск для массива long
     public static int binarySearch(long[] a, long key) {
         return genericBinarySearch(a, 0, a.length, key);
     }
 
+    // Бинарный поиск для части массива long
     public static int binarySearch(long[] a, int fromIndex, int toIndex, long key) {
         return genericBinarySearch(a, fromIndex, toIndex, key);
     }
 
+    // Бинарный поиск для массива short
     public static int binarySearch(short[] a, short key) {
         return genericBinarySearch(a, 0, a.length, key);
     }
 
+    // Бинарный поиск для части массива short
     public static int binarySearch(short[] a, int fromIndex, int toIndex, short key) {
         return genericBinarySearch(a, fromIndex, toIndex, key);
     }
 
+    // Бинарный поиск для обобщенных массивов с использованием Comparator
     public static <T> int binarySearch(T[] a, T key, Comparator<? super T> c) {
         return genericBinarySearch(a, 0, a.length, key, c);
     }
 
+    // Бинарный поиск для части обобщенного массива с использованием Comparator
     public static <T> int binarySearch(T[] a, int fromIndex, int toIndex, T key, Comparator<? super T> c) {
         return genericBinarySearch(a, fromIndex, toIndex, key, c);
     }
